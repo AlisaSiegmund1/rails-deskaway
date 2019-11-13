73053f640708e70340aebcd063f0d5d0dc17f711
Siya Ndiki <siyandiki@gmail.com>
2019-11-13 12:58:13 +0100

    deskaway wokspaces pundit authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #has_many :workspaces
+  has_many :workspaces
 end
