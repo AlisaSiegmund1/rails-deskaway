40d7a1334632c1d31c9eb313c60503a4b4783549
Iris Vermeulen <iris.m.vermeulen@gmail.com>
2019-11-13 16:00:38 +0100

    card for the index are there

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -6,7 +6,7 @@ class WorkspacesController < ApplicationController
 
   def index
     @workspaces = Workspace.all
-  #@workspace = policy_scope(Workspace).order(created_at: :asc) #workspace policy has scope.all , set the order of the workspaces
+  @workspace = policy_scope(Workspace).order(created_at: :asc) #workspace policy has scope.all , set the order of the workspaces
   end
 
   def new
