46e3f96613281594ebf0d9fd91d3c95e42c32dc4
Iris Vermeulen <iris.m.vermeulen@gmail.com>
2019-11-14 15:55:14 +0100

    Booking done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  get :profile, to: 'pages#profile'
+
   resources :workspaces do
     resources :bookings, only: [:new, :create]
   end
