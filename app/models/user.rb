64e59d188462c697f5ad5f60c010349b99734235
Alisa Siegmund <alisa.siegmund@gmail.com>
2019-11-15 00:32:11 +0100

    basic Design for profil page, added profile.scss

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :workspaces
+  has_many :bookings
   mount_uploader :photo, PhotoUploader
 end
